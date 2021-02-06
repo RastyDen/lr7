50dcd5fb7ec7721be37079c568914c09d4eb81fe
RastyDen <70960711+RastyDen@users.noreply.github.com>
2021-02-06 09:56:04 +0300

    Update 1ind.py

--- 1ind.py
+++ 1ind.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 # Написать программу, которая считывает текст из файла и выводит на экран только
 # предложения, начинающиеся с тире, перед которым могут находиться только пробельные
 # символы.
@@ -10,12 +11,19 @@ import sys
 if __name__ == '__main__':
     with open('ind1.txt', 'r', encoding="utf8") as f:
         text = f.read()
+
+    # Заменить символы конца предложения.
     text = text.replace("!", ".")
     text = text.replace("?", ".")
-    sentences: List[str] = text.split('.')
+
+    # Разбить текст на предложения.
+    sentences = text.split('.')
+
+    # Удаление пробельных символов.
     sentences = [i.strip() for i in sentences]
 
     # Вывод предложений начинающихся с тире.
     for sentence in sentences:
-        if '-' in sentence[:1]:
+        if '-' in sentence[:1]:  # или if sentence.startswith('-')
             print(sentence)
+
